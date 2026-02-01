165351b85d9ffe9e68e4fd34260791d09d27a5f9
carlos738 <carlosasl135@hotmail.com>
2026-02-01 20:55:40 -0300

    inicio do projeto

---
+++ ClasseDeJogoEmJS/Heroi.js
@@ -0,0 +1,31 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+  atacar() {
+    let ataque;
+    switch (this.tipo.toLowerCase()) {
+      case "mago":
+        ataque = "magia";
+
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "um ataque desconhecido";
+    }
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+// exemplo de uso
+const heroi1 = new Heroi("Arthas", 30, "guerreiro");
+heroi1.atacar();
